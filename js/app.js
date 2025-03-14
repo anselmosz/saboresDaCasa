6f03334b24b6f464590e1404f16875ddc70f7809
anselmosz <anselmoma2005@outlook.com>
2025-03-13 22:17:47 -0300

    update receita.html and css / added JS logic to lists

---
+++ js/app.js
@@ -0,0 +1,17 @@
+const ingredienteBtn = document.getElementById("ingrdienteBtn");
+const preparoBtn = document.getElementById("preparoBtn");
+const ingredientesContentID = document.getElementById("ingredientesContent");
+const preparoContentID = document.getElementById("preparoContent");
+
+preparoContentID.style.display = "none"
+
+function trocarLista(activeList, disableList) {
+  activeList.style.display = "block";
+  disableList.style.display = "none";
+}
+
+ingredienteBtn.addEventListener("click", ()=> { trocarLista(ingredientesContentID, preparoContentID) });
+
+preparoBtn.addEventListener("click", ()=> { trocarLista(preparoContentID, ingredientesContentID)})
+
+ingredientesContentID.style.back
